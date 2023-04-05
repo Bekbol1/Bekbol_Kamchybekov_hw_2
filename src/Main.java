5be6fa4ef637861da04d312a70e5ef094a3c305d
Bekbol1 <kbekbol9@gmail.com>
2023-04-05 15:36:27 +0600

    hw_2

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,11 @@ public class Main {
     }
 
     public static String canWalk(int age, int temperature) {
-        if (age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) {
+        if (age > 20 && age < 45 && temperature > -20 && temperature < 30) {
             return "Можно идти гулять";
-        } else if (age < 20 && temperature >= 0 && temperature <= 28) {
+        } else if (age < 20 && temperature > 0 && temperature < 28) {
             return "Можно идти гулять";
-        } else if (age > 45 && temperature >= -10 && temperature <= 25) {
+        } else if (age > 45 && temperature > -10 && temperature < 25) {
             return "Можно идти гулять";
         } else {
             return "Оставайтесь дома";
